5d5a5a49e0d82fbc28978d6bef13f2c7f20f7cc9
clagarejo <clagarejo89@gmail.com>
2021-07-07 16:17:31 -0500

    calculo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="name" type="text" placeholder="Escribe tu nombre"> 
+    <input v-model="lastName" type="text" placeholder="Escribe tu apellido"> 
+    <input v-model="birthDate" type="date">
+    <p>Hola {{ fullname }}, tu edad es: {{ age || '#' }} años</p>
+    <button @click="format">Formatear</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  data () {
+    return {
+      name: '',
+      lastName: '',
+      birthDate: '',
+    }
+  },
+
+  computed: {
+    fullname () {
+      return `${this.name} ${this.lastName}`
+    },
+    age () {
+      let diff = new Date() - new Date(this.birthDate)
+      let age = Math.floor(diff / 1000 / 60 / 60 / 24 / 365)
+      return age
+    }
+  },
+
+  watch: {
+    name (val, oldVal) {
+      console.log(val, oldVal);
+    },
+
+    lastName (val, oldVal) {
+      console.log(val, oldVal);
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    width: 60%;
+    background-color: #5D8233;
+    color: aliceblue;
+    padding: 50px;
+    margin: auto;
+    text-align: center;
+  }
+
+  input {
+    margin: 10px;
+    padding: 5px;
+    outline: none;
+  }
+
+
+
+
+/* @import './scss/main.scss'; */
+
 </style>
